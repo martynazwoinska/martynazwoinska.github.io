38a680e8185dba6448cf5c1aa948ddf0a606ef7f
Martyna Zwoinska <95091112+martynazwoinska@users.noreply.github.com>
2026-07-21 01:29:58 +0200

    Redraw Bristol river perspective

--- game-of-worms/game.js
+++ game-of-worms/game.js
@@ -2,7 +2,7 @@ import { geoGraticule10, geoNaturalEarth1, geoPath } from "https://cdn.jsdelivr.
 import { feature } from "https://cdn.jsdelivr.net/npm/topojson-client@3/+esm";
 import world from "https://esm.sh/@d3-maps/atlas@1.0.0/world/countries/countries-110m";
 import { createGameTranslator } from "./game-i18n.js?v=20260713-3";
-import { auditEnvironmentCompositions, getEnvironmentProfile, renderEnvironmentScene } from "./environment-scenes.js?v=20260721-1";
+import { auditEnvironmentCompositions, getEnvironmentProfile, renderEnvironmentScene } from "./environment-scenes.js?v=20260721-2";
 import { auditAccessoryCatalogue, auditAccessoryPairGeometry, renderLocationAccessories } from "./accessory-designs.js?v=20260718-1";
 
 const t = createGameTranslator(document.documentElement.lang);
